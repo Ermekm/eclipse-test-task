c955abe8bd8c6f2c3870bf078e1ab6441cc488ba
ermekm <ermek.mrzv@gmail.com>
2023-04-25 12:27:26 +0600

    fixed: github pages 404 error

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import * as ReactDOMClient from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store";
 
 ReactDOMClient.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/eclipse-test-task">
+  <HashRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 );
--- src/router/routeConfig.tsx
+++ src/router/routeConfig.tsx
@@ -1,16 +1,18 @@
 import PokemonList from "../pages/PokemonList";
 import PokemonDetails from "../pages/PokemonDetails";
 
-import { type RouteProps } from "react-router-dom";
+import { Navigate, type RouteProps } from "react-router-dom";
 
 export enum AppRoutes {
   POKEMON_LIST = "pokemonList",
   POKEMON_DETAILS = "pokemonDetails",
+  DEFAULT = "default",
 }
 
 export const RoutePath: Record<AppRoutes, string> = {
-  [AppRoutes.POKEMON_LIST]: "/",
-  [AppRoutes.POKEMON_DETAILS]: "/:name",
+  [AppRoutes.POKEMON_DETAILS]: "eclipse-test-task/:name",
+  [AppRoutes.POKEMON_LIST]: "eclipse-test-task",
+  [AppRoutes.DEFAULT]: "*",
 };
 
 export const routeConfig: Record<AppRoutes, RouteProps> = {
@@ -21,5 +23,9 @@ export const routeConfig: Record<AppRoutes, RouteProps> = {
   [AppRoutes.POKEMON_DETAILS]: {
     path: RoutePath.pokemonDetails,
     element: <PokemonDetails />,
+  },
+  [AppRoutes.DEFAULT]: {
+    path: RoutePath.default,
+    element: <Navigate to={RoutePath.pokemonList} />,
   },
 };
